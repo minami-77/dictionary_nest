3b5eede50aa256eb31a793651e99b89dbd6613d2
Minami Takayama <m.inam.ina03037@gmail.com>
2025-06-25 23:23:13 +0900

    add namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   devise_for :users
+  root to: 'pages#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :pages, only: [:index]
+    end
+  end
 end
