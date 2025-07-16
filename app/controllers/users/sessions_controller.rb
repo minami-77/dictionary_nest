9fb528be856d7547c7009d23e7a84bf6a9a0f6a9
Minami Takayama <m.inam.ina03037@gmail.com>
2025-07-16 17:34:33 +0900

    add create to sessions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,34 @@
 
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
+  def create
+    user = User.find_by(email: params[:user][:email])
+
+    # パスワードが正しいか確認
+    if user && user.valid_password?(params[:user][:password])
+      sign_in(user)  # Deviseのログイン処理
+      render json: {
+        status: { code: 200, message: 'Logged in successfully.' },
+        data: {
+          id: user.id,
+          email: user.email
+          # 必要ならトークンなどもここで返す
+        }
+      }, status: :ok
+    else
+      render json: {
+        status: { code: 401, message: 'Invalid email or password.' }
+      }, status: :unauthorized
+    end
+  end
+
+  # 🚪ログアウト処理
+  def destroy
+    sign_out(current_user)
+    render json: {
+      status: { code: 200, message: 'Logged out successfully.' }
+    }, status: :ok
+  end
 
   # before_action :configure_sign_in_params, only: [:create]
 
