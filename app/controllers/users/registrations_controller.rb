6880d94bf7f651b9fbbb685451cc465011f4311d
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-07-29 16:07:54 +0900

    delete method  in sessions and registrations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,25 +3,32 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
-  def create
-    user = User.new(sign_up_params)
-    if user.save
-      render json: {
-        status: { code: 200, message: 'Signed up successfully.' },
-        data: user
-      }, status: :ok
-    else
-      render json: {
-        status: { code: 422, message: "User couldn't be created successfully. #{user.errors.full_messages.to_sentence}" }
-      }, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   user = User.new(sign_up_params)
+  #   if user.save
+  #     render json: {
+  #       status: { code: 200, message: 'Signed up successfully.' },
+  #       data: user
+  #     }, status: :ok
+  #   else
+  #     render json: {
+  #       status: { code: 422, message: "User couldn't be created successfully. #{user.errors.full_messages.to_sentence}" }
+  #     }, status: :unprocessable_entity
+  #   end
+  # end
 
-  private
+  # private
 
-  def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
+  # def sign_up_params
+  #   params.require(:user).permit(:email, :password, :password_confirmation)
+  # end
+
+
+
+
+
+
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,37 +2,40 @@
 
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  # Log in
-  def create
-    # set variable user with params[:user][:email]
-    user = User.find_by(email: params[:user][:email])
 
-    # check password
-    if user && user.valid_password?(params[:user][:password])
-      # Deviseのログイン処理
-      sign_in(user)
-        render json: {
-        status: { code: 200, message: 'Logged in successfully.' },
-        data: {
-          id: user.id,
-          email: user.email
-          # 必要ならトークンなどもここで返す
-        }
-      }, status: :ok
-    else
-      render json: {
-        status: { code: 401, message: 'Invalid email or password.' }
-      }, status: :unauthorized
-    end
-  end
 
-  # ログアウト処理
-  def destroy
-    sign_out(current_user)
-    render json: {
-      status: { code: 200, message: 'Logged out successfully.' }
-    }, status: :ok
-  end
+  # # Log in
+  # def create
+  #   # set variable user with params[:user][:email]
+  #   user = User.find_by(email: params[:user][:email])
+
+  #   # check password
+  #   if user && user.valid_password?(params[:user][:password])
+  #     # Deviseのログイン処理
+  #     sign_in(user)
+  #       render json: {
+  #       status: { code: 200, message: 'Logged in successfully.' },
+  #       data: {
+  #         id: user.id,
+  #         email: user.email
+  #         # 必要ならトークンなどもここで返す
+  #       }
+  #     }, status: :ok
+  #   else
+  #     render json: {
+  #       status: { code: 401, message: 'Invalid email or password.' }
+  #     }, status: :unauthorized
+  #   end
+  # end
+
+  # # ログアウト処理
+  # def destroy
+  #   sign_out(current_user)
+  #   render json: {
+  #     status: { code: 200, message: 'Logged out successfully.' }
+  #   }, status: :ok
+  # end
+
 
   # before_action :configure_sign_in_params, only: [:create]
 
