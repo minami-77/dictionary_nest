71ed019e9432e5b820b57fd15a51461c54907c51
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-07-30 23:45:24 +0900

    changed registrations controller using respond with

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,32 +2,20 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
+  private
 
-  # def create
-  #   user = User.new(sign_up_params)
-  #   if user.save
-  #     render json: {
-  #       status: { code: 200, message: 'Signed up successfully.' },
-  #       data: user
-  #     }, status: :ok
-  #   else
-  #     render json: {
-  #       status: { code: 422, message: "User couldn't be created successfully. #{user.errors.full_messages.to_sentence}" }
-  #     }, status: :unprocessable_entity
-  #   end
-  # end
-
-  # private
-
-  # def sign_up_params
-  #   params.require(:user).permit(:email, :password, :password_confirmation)
-  # end
-
-
-
-
-
-
+    def respond_with(current_user, _opts = {})
+      if resource.persisted?
+        render json: {
+          status: {code: 200, message: 'Signed up successfully.'},
+          data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+        }
+      else
+        render json: {
+          status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+        }, status: :unprocessable_entity
+      end
+    end
 
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
