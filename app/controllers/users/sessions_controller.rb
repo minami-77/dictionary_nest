7f859ebeb517ac79228dc584b96382a6d559d442
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-08-01 19:01:39 +0900

    override sessions controler

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,38 +3,36 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
+  private
 
-  # # Log in
-  # def create
-  #   # set variable user with params[:user][:email]
-  #   user = User.find_by(email: params[:user][:email])
+  def respond_with(current_user, _opts = {})
+    render json: {
+      status: {
+        code: 200, message: 'Logged in successfully.',
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+      }
+    }, status: :ok
+  end
 
-  #   # check password
-  #   if user && user.valid_password?(params[:user][:password])
-  #     # Deviseのログイン処理
-  #     sign_in(user)
-  #       render json: {
-  #       status: { code: 200, message: 'Logged in successfully.' },
-  #       data: {
-  #         id: user.id,
-  #         email: user.email
-  #         # 必要ならトークンなどもここで返す
-  #       }
-  #     }, status: :ok
-  #   else
-  #     render json: {
-  #       status: { code: 401, message: 'Invalid email or password.' }
-  #     }, status: :unauthorized
-  #   end
-  # end
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
 
-  # # ログアウト処理
-  # def destroy
-  #   sign_out(current_user)
-  #   render json: {
-  #     status: { code: 200, message: 'Logged out successfully.' }
-  #   }, status: :ok
-  # end
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
+  end
+
 
 
   # before_action :configure_sign_in_params, only: [:create]
