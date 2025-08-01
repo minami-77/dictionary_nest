c1b384768724d55d4f690bd7109311e6b03572d9
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-08-01 19:43:09 +0900

    add strong -param

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,6 +21,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
 
+# strong params
+    def sign_up_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    end
+
+    def account_update_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :name)
+    end
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
