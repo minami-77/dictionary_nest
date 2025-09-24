8fc5753901ae3e4f215f0432286c6039499c525d
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-09-24 22:56:52 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :pages, only: [:index]
+      resources :words, only: [:index]
     end
   end
 end
