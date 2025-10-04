5961a5dadafcac72160c650502363df2d8ff05a8
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-04 19:33:14 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :pages, only: [:index]
       resources :words, only: [:index]
+      get "users/me", to: "users#me"
     end
   end
 end
