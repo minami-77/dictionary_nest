df051ab5b9b2b288e4f60fe5493e4babc4af7eec
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-06 17:45:21 +0900

    fixed config for jwt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -314,11 +314,15 @@ Devise.setup do |config|
   # To use jwt
   config.jwt do |jwt|
       jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+      # Requests for publishing JWT
       jwt.dispatch_requests = [
-        ['POST', %r{^/sign_in$}]
+        ['POST', %r{^/api/v1/signup$}],
+        ['POST', %r{^/api/v1/login$}]
+
       ]
+      # Requests for discarding JWT
       jwt.revocation_requests = [
-        ['DELETE', %r{^/sign_out$}]
+        ['DELETE', %r{^/api/v1/logout$}]
       ]
       jwt.expiration_time = 30.minutes.to_i
   end
