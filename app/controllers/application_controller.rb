7374ac045ca43e0dc8644ecbe441f32272b5a36e
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-06 23:38:24 +0900

    add helper to appC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
 # To use Devise's helper method
-  include Devise::Controllers::Helpers   # ✅ Deviseのhelperを明示的にinclude
-  include ActionController::MimeResponds  # ✅ render json: がうまく動かない時に必要な場合も
+# Devise + API モード対応
+  include ActionController::MimeResponds
+  include ActionController::Helpers
+  include Devise::Controllers::Helpers
 end
