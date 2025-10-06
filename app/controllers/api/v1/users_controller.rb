96811bf0d1e3b62ebc8b49f1833c82722c1f5ee3
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-06 23:58:15 +0900

    some fixes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::UsersController < ApplicationController
+  # To use Devise's helper method
+  include Devise::Controllers::Helpers
+
   before_action :authenticate_user!
   def me
     render json: {user: current_user}
