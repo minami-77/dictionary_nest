b14716fc5ef5bbb4f89c26b56da76091528837ce
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-07 23:34:09 +0900

    add appC name parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::API
 # To use Devise's helper method
   include Devise::Controllers::Helpers
 
-  # alias to use shorter method names in controllers
+# To allow name parameter
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
+  end
+
+# alias to use shorter method names in controllers
   alias_method :authenticate_user!, :authenticate_api_v1_user!
   alias_method :current_user, :current_api_v1_user
 
