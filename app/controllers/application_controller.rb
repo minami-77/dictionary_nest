4b1f147f50e3a32adc402abf0e8244057b82a1d4
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-07 14:42:19 +0900

    added alias

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include ActionController::Helpers
   include Devise::Controllers::Helpers
+
+  # alias to use shorter method names in controllers
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :current_user, :current_api_v1_user
+
 end
