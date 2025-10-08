687361d7304afa062aa263632f86b446898815ab
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-08 14:00:04 +0900

    add _api_v1

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::UsersController < ApplicationController
   # To use Devise's helper method
   # include Devise::Controllers::Helpers
 
-  before_action :authenticate_user!
+  before_action :authenticate_api_v1_user!
 
   def me
-    render json: {user: current_user}
+    render json: {user: current_api_v1_user}
   end
 
 end
