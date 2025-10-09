4f7e5f606c6088126badb84938a4c773bb5fff85
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-09 17:01:24 +0900

    add api_v1

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -8,15 +8,15 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   # override original respond_with method (in create method)
 
-    def respond_with(current_user, _opts = {})
+    def respond_with(current_api_v1_user, _opts = {})
       if resource.persisted?
         render json: {
           status: {code: 200, message: 'Signed up successfully.'},
-          data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+          data: UserSerializer.new(current_api_v1_user).serializable_hash[:data][:attributes]
         }
       else
         render json: {
-          status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+          status: {message: "User couldn't be created successfully. #{current_api_v1_user.errors.full_messages.to_sentence}"}
         }, status: :unprocessable_entity
       end
     end
