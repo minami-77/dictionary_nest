81a943dce448aca91738e30cc9508087aac55aa5
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-11 22:17:22 +0900

    deleted wordsAPI

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::WordsController < ApplicationController
-  require 'net/http'
-  require 'json'
 
   # receive searched words
   def index
@@ -11,17 +9,11 @@ class Api::V1::WordsController < ApplicationController
   end
 
   def search
-    # call WordsAPI
-
-    # These code snippets use an open-source library. http://unirest.io/ruby
-    response = Unirest.get "https://wordsapiv1.p.mashape.com/words/#{params[:query]}",
-    headers:{
-      "X-Mashape-Key" => '******************************35d02',
-      "Accept" => "application/json"
-    }
-
     render json:{
-      word: response.read_body
+
+
+
+
     }
 
   end
