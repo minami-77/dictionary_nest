8f01d667e1bc97d912d3bda30aab94fdf511411d
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-13 21:46:25 +0900

    drop meanings model migration

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -18,19 +18,41 @@ class Api::V1::WordsController < ApplicationController
       language: "en"
       )
 
-    if @word.save
+    # save parts of speech and definitions
+    whole_word_data = params[:word_data]
+    whole_word_data.each do |word_data|
+      word_data[:meanings].each do |meaning|
+        meaning = @word.part_of_speeches.create(
+          part_of_speech: meanings[:partOfSpeech]
+        )
+        category[:definitions].each do |definition|
+          part_of_speech.definitions.create(
+            description: definition[:definition],
+            example: definition[:example],
+            synonyms: definition[:synonyms].join(", ") ,
+            antonyms: definition[:antonyms].join(", ")
+          )
+        end
+      end
+    end
+
+
+    if @word.save && @part_of_speech.save && @definition.save
       render json: {
         status: 'SUCCESS',
         message: 'Saved the word',
-        data: @word
+        word: @word,
+        part_of_speech: @word.part_of_speech,
+        definition: @word.part_of_speech.definition
       }, status: :ok
     else
       render json: {
         status: 'ERROR',
         message: 'Did not save the word',
-        data: @word.errors
+        word: @word.errors
       }, status: :unprocessable_entity
     end
+
   end
 
 end
