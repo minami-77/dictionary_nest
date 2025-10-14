d01623266b8e584653ee1ef2f725090e83ee1fb1
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-14 16:45:14 +0900

    create method editing

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -12,52 +12,81 @@ class Api::V1::WordsController < ApplicationController
     # receive word data from frontend
     word_data = params[:word_data][:searchedResults]
 
-    @word = Word.new(
-      spelling: word_data[0][:word],
-      pronunciation: word_data[0][:phonetic],
-      language: "en"
+    # save the word if not saved yet
+    @word = Word.find_by(
+      spelling: word_data[0][:word])
+    if @word
+      render json: {
+        message: 'Word already saved',
+      } and return
+    else
+      @word = Word.create(
+        spelling: word_data[0][:word],
+        pronunciation: word_data[0][:phonetic],
+        language: "en"
       )
+    end
 
-    # save part of speeches and definitions
-    word_data.each do |data|
-      data[:meanings].each do |meaning_data|
-        part_of_speech = @word.part_of_speeches.new(
-          part_of_speech: meaning_data[:partOfSpeech]
-        )
-        category[:definitions].each do |def_data|
-          part_of_speech.definitions.new(
-            description: def_data[:definition],
-            example: def_data[:example]||"",
-            synonyms: def_data[:synonyms]||[],
-            antonyms: def_data[:antonyms]||[]
+    # save part of speeches and definitions if not saved yet
+    if @word.part_of_speeches.empty?
+      word_data.each do |data|
+        data[:meanings].each do |meaning_data|
+          part_of_speech = @word.part_of_speeches.create(
+            part_of_speech: meaning_data[:partOfSpeech]
           )
+          meaning_data[:definitions].each do |def_data|
+            part_of_speech.definitions.create(
+              description: def_data[:definition],
+              example: def_data[:example]||"",
+              synonyms: def_data[:synonyms]||[],
+              antonyms: def_data[:antonyms]||[]
+            )
+          end
         end
       end
+
+    else
+      render json: {
+        message: 'Part of speeches already saved',
+      } and return
     end
 
     # Connect user to the word
+    user = current_api_v1_user
+    user_word = user.user_words.find_by(word_id: @word.id)
 
-
-
-
-
-    # Display result
-    if @word.save && @part_of_speech.save && @definition.save
+    if user_word.persisted?
       render json: {
-        status: 'SUCCESS',
-        message: 'Saved the word',
-        word: @word,
-        part_of_speech: @word.part_of_speeches,
-        definition: @word.part_of_speech.definition
-      }, status: :ok
+        status: 'INFO',
+        message: 'Word already saved',
+        }, status: :ok
     else
-      render json: {
-        status: 'ERROR',
-        message: 'Did not save the word',
-        word: @word.errors
-      }, status: :unprocessable_entity
+      user_word = user.user_words.create(word_id: @word.id)
+      unless user_word.persisted?
+        render json: {
+          status: 'ERROR',
+          message: 'Did not connect the word to the user',
+          user: user.errors
+        }, status: :unprocessable_entity and return
+      end
     end
 
   end
-
 end
+
+    # # Display result
+    # if @word.persisted? && @word.part_of_speeches.present? && user_word.persisted?
+    #   render json: {
+    #     status: 'SUCCESS',
+    #     message: 'Saved the word',
+    #     word: @word,
+    #     part_of_speech: @word.part_of_speeches,
+    #     definition: @word.part_of_speech.definition
+    #   }, status: :ok
+    # else
+    #   render json: {
+    #     status: 'ERROR',
+    #     message: 'Did not save the word',
+    #     word: @word.errors
+    #   }, status: :unprocessable_entity
+    # end
