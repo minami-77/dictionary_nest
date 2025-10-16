436406dc308a56180413400b0d937671a475b313
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-16 17:04:46 +0900

    roll back CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # React(Vite)
-    origins "http://localhost:5173","https://api.dictionaryapi.dev"
+    origins "http://localhost:5173"
 
     resource "*",
       headers: :any,
