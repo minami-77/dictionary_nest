d7cc8792fe81381ee40f2af691ff6603e357cb88
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-29 23:43:52 +0900

    able to display data table userword

--- app/controllers/api/v1/user_words_controller.rb
+++ app/controllers/api/v1/user_words_controller.rb
@@ -3,31 +3,46 @@ class Api::V1::UserWordsController < ApplicationController
 
   def index
     user = current_api_v1_user
-    user_words = user.user_words.includes(word: {part_of_speech: :definition})
+    # includes(parent:{children::grandchildren})
+    user_words = user.user_words.includes(word: {part_of_speeches: :definitions})
+
+    Rails.logger.debug "===== UserWords Debug ====="
+    user_words.each do |uw|
+      Rails.logger.debug "UserWord ID: #{uw.id}"
+      Rails.logger.debug "Word: #{uw.word.inspect}"
+      Rails.logger.debug "PartOfSpeeches: #{uw.word.part_of_speeches.inspect}"
+      Rails.logger.debug "First PartOfSpeech: #{uw.word.part_of_speeches.first&.inspect}"
+      Rails.logger.debug "Definitions: #{uw.word.part_of_speeches.first&.definitions&.inspect}"
+    end
+    Rails.logger.debug "=========================="
 
     render json: {
       status: 'SUCCESS',
+      # .map will return a hash directly (no variables)
+      # shadcn's Data table receive flatten data only (no nest allowed)
       data: user_words.map do |uw|
         {
-          id: uw.id,
-          note: note,
-          name: uw.word.name,
-          word: uw.word.spelling,
+          note: uw.note,
+          spelling: uw.word.spelling,
           pronunciation: uw.word.pronunciation,
-          part_of_speeches:
-            uw.part_of_speech.map do |pos|
-              {
-                part_of_speech: pos.part_of_speech
-              }
-              pos.map do |definition|
-                {
-                  definition: definition.definition,
-                  example: definition.example,
-                  synonyms: [definition.synonyms.join(",")],
-                  antonyms: [definition.antonyms.join(",")]
-                }
-              end
-          end
+          part_of_speech: uw.word.part_of_speeches.first&.part_of_speech,
+          definition: uw.word.part_of_speeches&.first&.definitions&.first&.definition,
+          example: uw.word.part_of_speeches&.first&.definitions&.first&.example
+
+          # part_of_speeches:
+          #   uw.part_of_speeches.map do |pos|
+          #     {
+          #       part_of_speech: pos.part_of_speech
+          #     }
+          #     pos.map do |definition|
+          #       {
+          #         definition: definition.definition,
+          #         example: definition.example,
+          #         synonyms: [definition.synonyms.join(",")],
+          #         antonyms: [definition.antonyms.join(",")]
+          #       }
+          #     end
+          # end
         }
 
       end
